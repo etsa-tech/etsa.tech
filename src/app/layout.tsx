40975f6518b06d537ae2bc987758d89ebde8f283
Wesley Kirkland <wesley@wesleyk.me>
2025-08-02 15:37:53 -0400

    Upgrade to Tailwind V4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
         className={`${inter.variable} font-sans antialiased bg-white dark:bg-etsa-dark text-gray-900 dark:text-gray-100`}
         suppressHydrationWarning
       >
-        <ThemeProvider defaultTheme="system" storageKey="etsa-ui-theme">
+        <ThemeProvider defaultTheme="light" storageKey="etsa-ui-theme">
           <div className="relative flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
